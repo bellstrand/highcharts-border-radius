a4ce703ec203650c86707712659489c8cbc0d811
Christopher Schick <c.schick@mail.de>
2024-02-12 14:03:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,94 @@
-'use strict';
-(function(factory) {
-	if(typeof module === 'object' && module.exports) {
-		module.exports = factory;
-	} else {
-		factory(Highcharts);
-	}
-}(function(Highcharts) {
-	(function(H) {
-		H.wrap(H.seriesTypes.column.prototype, 'translate', function(proceed) {
-			const options = this.options;
-			const topMargin = options.topMargin || 0;
-			const bottomMargin = options.bottomMargin || 0;
+// ChartBorderRadius.js
+export default function (Highcharts) {
+  if (Highcharts._borderRadiusApplied) return
 
-			proceed.call(this);
+  // Markiere das Highcharts-Objekt, um zu signalisieren, dass das Modul angewendet wurde
+  Highcharts._borderRadiusApplied = true
+  ;(function (H) {
+    H.wrap(H.seriesTypes.column.prototype, 'translate', function (proceed) {
+      const options = this.options
+      const topMargin = options.topMargin || 0
+      const bottomMargin = options.bottomMargin || 0
 
-			this.points.forEach(function(point) {
-				if(options.borderRadiusTopLeft || options.borderRadiusTopRight || options.borderRadiusBottomRight || options.borderRadiusBottomLeft) {
-					const w = point.shapeArgs.width;
-					const h = point.shapeArgs.height;
-					const x = point.shapeArgs.x;
-					const y = point.shapeArgs.y;
+      proceed.call(this)
 
-					var radiusTopLeft = H.relativeLength(options.borderRadiusTopLeft || 0, w);
-					var radiusTopRight = H.relativeLength(options.borderRadiusTopRight || 0, w);
-					var radiusBottomRight = H.relativeLength(options.borderRadiusBottomRight || 0, w);
-					var radiusBottomLeft = H.relativeLength(options.borderRadiusBottomLeft || 0, w);
+      this.points.forEach(function (point) {
+        if (
+          options.borderRadiusTopLeft ||
+          options.borderRadiusTopRight ||
+          options.borderRadiusBottomRight ||
+          options.borderRadiusBottomLeft
+        ) {
+          const w = point.shapeArgs.width
+          const h = point.shapeArgs.height
+          const x = point.shapeArgs.x
+          const y = point.shapeArgs.y
 
-					const maxR = Math.min(w, h) / 2
+          var radiusTopLeft = H.relativeLength(options.borderRadiusTopLeft || 0, w)
+          var radiusTopRight = H.relativeLength(options.borderRadiusTopRight || 0, w)
+          var radiusBottomRight = H.relativeLength(options.borderRadiusBottomRight || 0, w)
+          var radiusBottomLeft = H.relativeLength(options.borderRadiusBottomLeft || 0, w)
 
-					radiusTopLeft = radiusTopLeft > maxR ? maxR : radiusTopLeft;
-					radiusTopRight = radiusTopRight > maxR ? maxR : radiusTopRight;
-					radiusBottomRight = radiusBottomRight > maxR ? maxR : radiusBottomRight;
-					radiusBottomLeft = radiusBottomLeft > maxR ? maxR : radiusBottomLeft;
+          const maxR = Math.min(w, h) / 2
 
-					point.dlBox = point.shapeArgs;
+          radiusTopLeft = radiusTopLeft > maxR ? maxR : radiusTopLeft
+          radiusTopRight = radiusTopRight > maxR ? maxR : radiusTopRight
+          radiusBottomRight = radiusBottomRight > maxR ? maxR : radiusBottomRight
+          radiusBottomLeft = radiusBottomLeft > maxR ? maxR : radiusBottomLeft
 
-					point.shapeType = 'path';
-					point.shapeArgs = {
-						d: [
-							'M', x + radiusTopLeft, y + topMargin,
-							'L', x + w - radiusTopRight, y + topMargin,
-							'C', x + w - radiusTopRight / 2, y, x + w, y + radiusTopRight / 2, x + w, y + radiusTopRight,
-							'L', x + w, y + h - radiusBottomRight,
-							'C', x + w, y + h - radiusBottomRight / 2, x + w - radiusBottomRight / 2, y + h, x + w - radiusBottomRight, y + h + bottomMargin,
-							'L', x + radiusBottomLeft, y + h + bottomMargin,
-							'C', x + radiusBottomLeft / 2, y + h, x, y + h - radiusBottomLeft / 2, x, y + h - radiusBottomLeft,
-							'L', x, y + radiusTopLeft,
-							'C', x, y + radiusTopLeft / 2, x + radiusTopLeft / 2, y, x + radiusTopLeft, y,
-							'Z'
-						]
-					};
-				}
+          point.dlBox = point.shapeArgs
 
-			});
-		});
-	}(Highcharts));
-}));
+          point.shapeType = 'path'
+          point.shapeArgs = {
+            d: [
+              'M',
+              x + radiusTopLeft,
+              y + topMargin,
+              'L',
+              x + w - radiusTopRight,
+              y + topMargin,
+              'C',
+              x + w - radiusTopRight / 2,
+              y,
+              x + w,
+              y + radiusTopRight / 2,
+              x + w,
+              y + radiusTopRight,
+              'L',
+              x + w,
+              y + h - radiusBottomRight,
+              'C',
+              x + w,
+              y + h - radiusBottomRight / 2,
+              x + w - radiusBottomRight / 2,
+              y + h,
+              x + w - radiusBottomRight,
+              y + h + bottomMargin,
+              'L',
+              x + radiusBottomLeft,
+              y + h + bottomMargin,
+              'C',
+              x + radiusBottomLeft / 2,
+              y + h,
+              x,
+              y + h - radiusBottomLeft / 2,
+              x,
+              y + h - radiusBottomLeft,
+              'L',
+              x,
+              y + radiusTopLeft,
+              'C',
+              x,
+              y + radiusTopLeft / 2,
+              x + radiusTopLeft / 2,
+              y,
+              x + radiusTopLeft,
+              y,
+              'Z',
+            ],
+          }
+        }
+      })
+    })
+  })(Highcharts)
+}
